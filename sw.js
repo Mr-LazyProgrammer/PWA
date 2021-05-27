7f9fafb307019e77ddfac33a60d4b38bc7a6a0a2
Smruti Ranjan Badatya <35039502+Mr-LazyProgrammer@users.noreply.github.com>
2021-05-28 02:03:14 +0530

    final

--- sw.js
+++ sw.js
@@ -7,26 +7,6 @@ self.addEventListener('install', e => {
     caches.open(cacheName).then(cache => {
       return cache.addAll([
         '/PWA/',
-        '/PWA/index.html',
-        '/PWA/style.css',
-        '/PWA/assets/aditi.webp',
-        '/PWA/assets/allipsa.webp',
-        '/PWA/assets/ansu.webp',
-        '/PWA/assets/arvind.webp',
-        '/PWA/assets/kuldeep.webp',
-        '/PWA/assets/lalit.webp',
-        '/PWA/assets/logo.webp',
-        '/PWA/assets/phone11.webp',
-        '/PWA/assets/phone12.webp',
-        '/PWA/assets/smruti.webp',
-        '/PWA/assets/topButton.webp',
-        '/PWA/icons1/android-chrome-192x192.png',
-        '/PWA/icons1/android-chrome-512x512.png',
-        '/PWA/icons1/mstile-150x150.png',
-        '/PWA/icons1/apple-touch-icon.png',
-        '/PWA/icons1/favicon-16x16.png',
-        '/PWA/icons1/favicon-32x32.png',
-        '/PWA/icons1/safari-pinned-tab.svg'
       ]).then(() => self.skipWaiting());
     })
   );
